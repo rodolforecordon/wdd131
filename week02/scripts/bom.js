b7119823d374fc79fdd0cca68bda0842434e6b4a
Rodolfo Recordon <rodolforecordon@gmail.com>
2025-09-15 23:16:07 +0100

    BoM javascript activity first commit

---
+++ week02/scripts/bom.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;

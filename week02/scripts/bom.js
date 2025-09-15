cbdd1330550df17532ff13511fbcf088f9b00c60
Rodolfo Recordon <rodolforecordon@gmail.com>
2025-09-15 23:49:31 +0100

    BoM activity - add event listeners to insert and delete chapters

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,7 +1,28 @@
-const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
+const input = document.querySelector('#favchap');
+const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
+button.addEventListener('click', function () {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+
+    li.append(deleteButton);
+    list.append(li);
+
+    input.value = '';
+    input.focus();
+  }
+  input.focus();
+});
+
+list.addEventListener('click', function (e) {
+  if (e.target.classList.contains('delete')) {
+    e.target.parentElement.remove();
+    input.focus();
+  }
+});
